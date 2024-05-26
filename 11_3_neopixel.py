0638633bac2082dfad48485be1509e4f18128b04
javacasm <javacasm@gmail.com>
2024-05-26 19:45:44 +0200

    udpate

--- 11_3_neopixel.py
+++ 11_3_neopixel.py
@@ -1,12 +1,13 @@
-import array, time
+import array
+from time import sleep_ms
 from machine import Pin
 import rp2
 from random import randint
 
 NUM_LEDS = 64
-MAX_BRILLO = 10
+MAX_BRILLO = 50
 
-# Start of Magic programmable IO code
+# Inicio del código mágico para Neopixels usando PIO
 @rp2.asm_pio(sideset_init=rp2.PIO.OUT_LOW, out_shiftdir=rp2.PIO.SHIFT_LEFT, autopull=True, pull_thresh=24)
 def ws2812():
     T1 = 2
@@ -22,9 +23,9 @@ def ws2812():
     wrap()
 
 sm = rp2.StateMachine(0, ws2812, freq=8_000_000, sideset_base=Pin(22))
-sm.active(1) # Start the StateMachine
+sm.active(1) # Activamos la máquina de estados
 pixels = array.array("I", [0 for _ in range(NUM_LEDS)])
-# End of Magic programmable IO code
+# Fin del código mágico para Neopixels usando PIO
 
 def set_led(led, red, green, blue):
     pixels[led] = (red << 16) + (green << 8) + blue
@@ -32,29 +33,35 @@ def set_led(led, red, green, blue):
 def show():
     sm.put(pixels, 8)
 
-def clear():
+def borrar():
     for i in range(NUM_LEDS):
         set_led(i, 0, 0, 0)
     show()
     
-def randomize():
-    clear()
+def aleatorio():
+    borrar()
     for i in range(NUM_LEDS):
         set_led(i, randint(0, MAX_BRILLO), randint(0, MAX_BRILLO), randint(0, MAX_BRILLO))
         show()
-        time.sleep(0.001)
+        sleep_ms(5)
     
-clear()
+borrar()
 
-print("Enter the LED's number to turn it on")
-print("or c-clear r-randomize q-exit")
+print("Teclea el número del LED que quieres encender")
+print("o b-borrar a-aleatorio s-salir")
 while True:
-    led_str = input("command: ")
-    if (led_str == 'c'):
-        clear()
-    elif (led_str == 'r'):
-        randomize()
+    comando = input("comando: ")
+    if (comando == 'b'):
+        borrar()
+    elif (comando == 'a'):
+        aleatorio()
+    elif (comando == 's'):
+        print('Adiós')
+        break
     else:
-        led = int(led_str)
-        set_led(led, MAX_BRILLO, MAX_BRILLO, MAX_BRILLO) # white
-        show()
+        try:
+            led = int(comando)
+            set_led(led, MAX_BRILLO, MAX_BRILLO, MAX_BRILLO) # Blanco
+            show()
+        except:
+            pass
